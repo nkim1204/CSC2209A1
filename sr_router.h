05f17d5b1d16b6601e29314eb1dbeec43f417cea
Nick Kim <nkim1204@gmail.com>
2012-10-11 02:44:40 -0400

    working set

--- sr_router.h
+++ sr_router.h
@@ -72,4 +72,6 @@ void sr_set_ether_ip(struct sr_instance* , uint32_t );
 void sr_set_ether_addr(struct sr_instance* , const unsigned char* );
 void sr_print_if_list(struct sr_instance* );
 
+uint16_t compute_checksum(uint8_t* header, int length);
+
 #endif /* SR_ROUTER_H */
